a638feb79cb3629d2f3efc41624bf538edc1f484
William Chang <william@creativecrew.org>
2008-09-30 04:59:33 +0000

    Added preprocessor condition switch between _WINDOWS and _CONSOLE.

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,7 +4,7 @@
 extern "C" {
 #endif
 
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32 && _WINDOWS
 #define WIN32_LEAN_AND_MEAN
 #include "windows.h"
 INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
          // Application provides a go method, which starts the rendering.
         app.go();
     } catch( Ogre::Exception& e ) {
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32 && _WINDOWS
         MessageBoxA(NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
 #else
         std::cerr << "An exception has occured: " << e.getFullDescription().c_str() << std::endl;
